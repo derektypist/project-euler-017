9fd6f356d4d87fe88394850ab0152a206ca12f57
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-18 11:45:12 +0000

    Updated constant of SHORTSCALE at script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const UNITS = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
 const TENS = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-const SHORTSCALE = ['','thousand','million','billion','trillion','quadrillion','quintillion'];
+const SHORTSCALE = ['','thousand','million'];
 
 // Function to Get Number Information (including invalid Input)
 function getNumberInfo() {
