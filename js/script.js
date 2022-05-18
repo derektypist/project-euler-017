cd2a7fed7fd1f2a09b946f30463d4d3b79019104
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-18 09:51:26 +0000

    Added global constants at script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Set Up Global Constants
+
+const UNITS = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+const TENS = ['','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+const SHORTSCALE = ['','thousand','million','billion','trillion','quadrillion','quintillion'];
+
+// Function to Get Number Information (including invalid Input)
