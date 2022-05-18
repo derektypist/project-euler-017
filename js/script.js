b096121c04f2d0e562dd5dbfc6c365d304f2dcc5
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-18 11:23:35 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ function getNumberInfo() {
     // Get the Value of the Input Field
     let num = document.getElementById("mynumber").value;
     // Check if input is valid
-    if (isNaN(num) || num.toString().length == 0 || num < 1 || num>1e32 || !Number.isInteger(Number(num))) {
-        txt += `Invalid Input.  Please enter a whole number between 1 and 100000000000000000000000000000000.`;
+    if (isNaN(num) || num.toString().length == 0 || num < 1 || num>1000000 || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 1 and 1000000.`;
     } else {
         txt += `You have entered the number ${num}.<p>`;
         txt += `${numberLetterCounts(num)} letters have been used for writing out these numbers in words.`
