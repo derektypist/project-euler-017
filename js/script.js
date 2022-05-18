c2184dc1a1ba7e379f1975b3cb2859c4fb3bfaad
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-18 09:59:25 +0000

    Added getNumberInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ const TENS = ['','twenty','thirty','forty','fifty','sixty','seventy','eighty','n
 const SHORTSCALE = ['','thousand','million','billion','trillion','quadrillion','quintillion'];
 
 // Function to Get Number Information (including invalid Input)
+function getNumberInfo() {
+    // Set Up Variable
+    let txt = "";
+    // Get the Value of the Input Field
+    let num = document.getElementById("mynumber").value;
+    // Check if input is valid
+    if (isNaN(num) || num.toString().length == 0 || num < 1 || num>1e32 || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 1 and 100000000000000000000000000000000.`;
+    } else {
+        txt += `You have entered the number ${num}.<p>`;
+        txt += `${numberLetterCounts(num)} letters have been used for writing out these numbers in words.`
+    }
+
+    // Display Information in the Browser
+    document.getElementById("numinfo").innerHTML = txt;
+}
